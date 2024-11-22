23a2b1374c70b83a5107392baaaf930498eba2ab
sfomuseumbot <sfomuseumbot@localhost>
2024-11-22 10:01:27 -0800

    add pmtiles; workers

--- cmd/reverse-geocode/main.go
+++ cmd/reverse-geocode/main.go
@@ -6,12 +6,17 @@ import (
 	_ "fmt"
 	"log"
 	"log/slog"
+	"os"
+	"strconv"
+	"strings"
+	"sync"
 
-	// "github.com/aaronland/go-foursquare-places"
 	"github.com/paulmach/orb"
 	"github.com/paulmach/orb/geojson"
+	"github.com/sfomuseum/go-csvdict"
+	"github.com/whosonfirst/go-foursquare-places"
 	"github.com/whosonfirst/go-foursquare-places/emitter"
-	// "github.com/whosonfirst/go-reader"
+	_ "github.com/whosonfirst/go-whosonfirst-spatial-pmtiles"
 	"github.com/whosonfirst/go-whosonfirst-spatial/database"
 	"github.com/whosonfirst/go-whosonfirst-spatial/filter"
 	"github.com/whosonfirst/go-whosonfirst-spatial/hierarchy"
@@ -22,9 +27,11 @@ func main() {
 
 	var spatial_database_uri string
 	var emitter_uri string
+	var workers int
 
 	flag.StringVar(&spatial_database_uri, "spatial-database-uri", "", "...")
 	flag.StringVar(&emitter_uri, "emitter-uri", "", "")
+	flag.IntVar(&workers, "workers", 50, "...")
 
 	flag.Parse()
 
@@ -57,12 +64,61 @@ func main() {
 
 	resolver, err := hierarchy.NewPointInPolygonHierarchyResolver(ctx, resolver_opts)
 
-	for pl, err := range e.Emit(ctx) {
+	mu := new(sync.RWMutex)
+	wg := new(sync.WaitGroup)
 
-		if err != nil {
-			slog.Error("Failed to yield place", "error", err)
-			continue
-		}
+	throttle := make(chan bool, workers)
+
+	for i := 0; i < workers; i++ {
+		throttle <- true
+	}
+
+	var csv_wr *csvdict.Writer
+
+	process_place := func(ctx context.Context, pl *places.Place) error {
+
+		parent_id := int64(-1)
+		belongs_to := make([]int64, 0)
+
+		defer func() {
+
+			str_belongs_to := make([]string, len(belongs_to))
+
+			for i, id := range belongs_to {
+				str_belongs_to[i] = strconv.FormatInt(id, 10)
+			}
+
+			out := map[string]string{
+				"4sq:id":         pl.Id,
+				"wof:parent_id":  strconv.FormatInt(parent_id, 10),
+				"wof:belongs_to": strings.Join(str_belongs_to, ","),
+			}
+
+			mu.Lock()
+			defer mu.Unlock()
+
+			if csv_wr == nil {
+
+				fieldnames := make([]string, 0)
+
+				for k, _ := range out {
+					fieldnames = append(fieldnames, k)
+				}
+
+				wr, err := csvdict.NewWriter(os.Stdout, fieldnames)
+
+				if err != nil {
+					slog.Error("Failed to create CSV writer", "error", err)
+					return
+				}
+
+				csv_wr = wr
+				csv_wr.WriteHeader()
+			}
+
+			csv_wr.WriteRow(out)
+			csv_wr.Flush()
+		}()
 
 		lat := pl.Latitude
 		lon := pl.Longitude
@@ -80,23 +136,64 @@ func main() {
 
 		if err != nil {
 			slog.Error("Failed to marshal JSON", "error", err)
-			continue
+			return err
 		}
 
 		possible, err := resolver.PointInPolygon(ctx, inputs, body)
 
 		if err != nil {
 			slog.Error("Failed to resolve PIP", "error", err)
-			continue
+			return err
 		}
 
 		parent_spr, err := results_cb(ctx, spatial_db, body, possible)
 
 		if err != nil {
 			slog.Error("Failed to process results", "error", err)
+			return err
+		}
+
+		if parent_spr != nil {
+
+			p_id, err := strconv.ParseInt(parent_spr.Id(), 10, 64)
+
+			if err != nil {
+				slog.Error("Failed to parse parse parent ID", "id", parent_spr.Id(), "error", err)
+				return err
+			}
+
+			parent_id = p_id
+			belongs_to = parent_spr.BelongsTo()
+		}
+
+		return nil
+	}
+
+	for pl, err := range e.Emit(ctx) {
+
+		if err != nil {
+			slog.Error("Failed to yield place", "error", err)
 			continue
 		}
 
-		slog.Info("id", pl.Id, "parent", parent_spr)
+		<-throttle
+
+		wg.Add(1)
+
+		go func(pl *places.Place) {
+
+			defer func() {
+				throttle <- true
+				wg.Done()
+			}()
+
+			err = process_place(ctx, pl)
+
+			if err != nil {
+				slog.Error("Failed to process place", "error", err)
+			}
+		}(pl)
 	}
+
+	wg.Wait()
 }
